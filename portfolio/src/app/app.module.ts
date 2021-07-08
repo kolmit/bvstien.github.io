407dac5bd064d63e2a8dc9ea310848f235ddf517
bvstien <bastien.gouzerch@wanadoo.fr>
2021-07-08 22:52:36 +0200

    Init with CSS content :)

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
