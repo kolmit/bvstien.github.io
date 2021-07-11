88053d9f1bf1230d692ca35a0d84888a8c2db23d
bvstien <bastien.gouzerch@wanadoo.fr>
2021-07-12 01:44:41 +0200

    Some more CSS features

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    DashboardComponent,
     FeedComponent
   ],
   imports: [
